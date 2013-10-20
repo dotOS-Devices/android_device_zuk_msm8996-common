5affea96e992a199788ac05660d8a4239707a5fe
Arne Coucheron <arco68@gmail.com>
2013-10-20 05:51:28 +0200

    msm8996-common: Choose between Virtual or External WFD display
    * Choose between Virtual or External WFD display
    * persist.sys.wfd.virtual is used to distinguish, between WFD (Google API versus QCOM API)
    
    Change-Id: Ifc78a4ee58c6a91f9ef69f0c9553ac700371db57
    Signed-off-by: TogoFire <italomellopereira@gmail.com>
    Co-Authored-By: Arne Coucheron <519564+arco@users.noreply.github.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: ReallySnow <reallysnow233@gmail.com>
    
    # Conflicts:
    #	vendor_prop.mk

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -289,7 +289,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.gralloc.enable_fb_ubwc=1 \
     vendor.video.disable.ubwc=1 \
     persist.sys.wfd.nohdcp=1 \
-    debug.sf.enable_hwc_vds=1
+    debug.sf.enable_hwc_vds=1 \
+    persist.sys.wfd.virtual=0
 
 # Zygote preforking
 PRODUCT_PROPERTY_OVERRIDES += \
