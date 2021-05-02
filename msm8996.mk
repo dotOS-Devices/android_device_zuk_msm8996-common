dfc083cecdedcda563d5b482b34d5aafe5ddfcf7
ReallySnow <reallysnow233@gmail.com>
2021-05-02 20:20:46 +0000

    Revert "msm8996-common: Build vendor.lineage.livedisplay@2.0-service-sdm now"
    This reverts commit 6d5954a80474c305eb3aac053b6a0fe19417f383.

--- msm8996.mk
+++ msm8996.mk
@@ -293,10 +293,6 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
